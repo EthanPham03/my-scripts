084dcba1a2fbfeada01f42fd34aad91cf21fe2ea
Ethan Pham <ethanlepham@gmail.com>
2024-02-22 18:51:27 -0600

    ops-challenge:class04.sh

---
+++ class04-warmup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Script Name:                  ops201-challenge02-my-first-bash-script
+# Author:                       Ethan Pham
+# Date of latest revision:      02/20/2024
+# Purpose:                      Create a script that prints a string to the console
+#
+# Declaration of variables
+
+ask_for_name() {
+echo "please enter your name:"
+
+}
+
+
+# Main
+ask_for_name # this calls a function
+
+
+# End
