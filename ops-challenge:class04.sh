636b98ef592cbe06b86300e4beaff1d1d9a44b38
Ethan Pham <ethanlepham@gmail.com>
2024-02-22 19:39:51 -0600

    ops-challenge:class04.sh

---
+++ ops-challenge:class04.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Script Name:                  ops201-challenge04-Arrays
+# Author:                       Ethan Pham
+# Date of latest revision:      02/22/2024
+# Purpose:                      Write a script that: creates four directions (dir1, dir2, dir3, dir4); put the names of the four directions in an array; references the array variable to create a new .txt file in each directory. 
+
+# Declaration of variables
+directories=("dir1" "dir2" "dir3" "dir4")
+
+mkdir {"dir1"}
+mkdir {"dir2"}
+mkdir {"dir3"}
+mkdir {"dir4"}
+
+echo $dir1
+echo $dir2
+echo $dir3
+echo $dir4
+
+echo > /home/dir1/dir1.txt
+echo > /home/dir2/dir2.txt
+echo > /home/dir3/dir3.txt
+echo > /home/dir4/dir4.txt
+
+touch "dir1.txt"
+touch "dir2.txt"
+touch "dir3.txt"
+touch "dir4.txt"
+
+# Declaration of functions
+
+
+
+
+# Main
+
+
+
+# End
