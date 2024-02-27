86844c5013cdce44aa8e29569f41a1120edc844a
Ethan Pham <ethanlepham@gmail.com>
2024-02-27 15:43:50 -0600

    ops-challenge:class06.sh

--- ops-challenge:class06.sh
+++ ops-challenge:class06.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Script Name:                  ops201-challenge02-my-first-bash-script
+# Script Name:                  ops201-challenge06-conditionals
 # Author:                       Ethan Pham
-# Date of latest revision:      02/20/2024
-# Purpose:                      Create a script that prints a string to the console
-#
+# Date of latest revision:      02/26/2024
+# Purpose:                      Create a scripe that detects if a file or directory exists, then creates it if it does not exist. 
+
 # Declaration of variables
 
 files=("dir1.txt" "dir2.txt" "dir3.txt" "dir4.txt")
