a962de6656e68077e1241ec1fb363909a5f85283
Ethan Pham <ethanlepham@gmail.com>
2024-02-27 15:45:33 -0600

    ops-challenge:class05.sh

--- ops-challenge:class05.sh
+++ ops-challenge:class05.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Script Name:                  ops201-challenge02-my-first-bash-script
+# Script Name:                  ops201-challenge05-loops
 # Author:                       Ethan Pham
-# Date of latest revision:      02/20/2024
-# Purpose:                      Create a script that prints a string to the console
+# Date of latest revision:      02/23/2024
+# Purpose:                      Write a script that displays running processes, asks the user for a PID, then kills the process with that PID.
 #
 # Declaration of variables
 pstree 
