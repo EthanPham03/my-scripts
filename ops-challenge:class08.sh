d59ecdce24a790cbc0dc3219a6f9b010e4453d56
Ethan Pham <ethanlepham@gmail.com>
2024-02-28 18:08:50 -0600

    ops-challenge:class08.sh

---
+++ ops-challenge:class08.sh
@@ -0,0 +1,15 @@
+touch C:\Users\<EthanPham>\Desktop\Ops201
+touch C:\Users\<EthanPham>\Desktop\Ops201\labs
+touch C:\Users\<EthanPham>\Desktop\Ops201\labs\data_restoration.md
+touch C:\Users\<EthanPham>\Desktop\Ops201\labs\cloud_virtualization.md
+touch C:\Users\<EthanPham>\Desktop\Ops201\labs\malware_remediation.md
+touch C:\Users\<EthanPham>\Desktop\Ops201\challenges
+touch C:\Users\<EthanPham>\Desktop\Ops201\challenges\bash_domain_analyzer.sh
+touch C:\Users\<EthanPham>\Desktop\Ops201\challenges\powershell_IP analysis.psl
+touch C:\Users\<EthanPham>\Desktop\Ops201\challengesautomated_endpoint_config.sh
+touch C:\Users\<EthanPham>\Desktop\Ops201\notes
+touch C:\Users\<EthanPham>\Desktop\Ops201\notes\imaging_backup_recovery.txt
+touch C:\Users\<EthanPham>\Desktop\Ops201\notes\workstation_deployment_SOP.txt
+touch C:\Users\<EthanPham>\Desktop\Ops201\notes\system_log_analysis.txt
+
+touch "RecursiveCopy.bat"
