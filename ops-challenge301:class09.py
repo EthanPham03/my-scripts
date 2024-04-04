ae88a94fc1519514b1546c90b0c305bbf3066979
Ethan Pham <ethanlepham@gmail.com>
2024-04-04 17:36:21 -0500

    ops-challenge301:class09.py

---
+++ ops-challenge301:class09.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+
+# Declaration of variables
+
+
+# Declaration of functions
+if a == b:
+    print("a equals b")
+else: 
+    print("a doesn't equal b")
+
+if a != b:
+    print("a doesn't equal b")
+else: 
+    print("a equals b")
+
+if a < b:
+    print("a is less than b")
+else:
+    print("a isn't less than b")
+
+if a <= b:
+    print("a is less than or equal to b")
+else:
+    print("a is greater than b")
+
+if a > b:
+    print("a is greater than b")
+else:
+    print("a isn't greater than b")
+
+if a >= b:
+    print("a is greater than or equal to b")
+else:
+    print("a is less than b")
+
+if kiryu > majima:
+    print("kiryu beats majima")
+elif kiryu < majima:
+    print("majima beats kiryu")
+else:
+    print("tie")
+
+
+
+# Main
+
+
+# End
