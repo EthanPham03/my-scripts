35bcb329353a7b92df182ca54a8159d4aa7f7868
Ethan Pham <ethanlepham@gmail.com>
2024-04-03 19:05:13 -0500

    ops-challenge301:class08.py

---
+++ ops-challenge301:class08.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+
+
+# Declaration of variables
+fruits = ["apple", "banana", "grape", "blueberry", "watermelon", "grapefruit", "mango", "rasberry", "strawberry", "orange"]
+
+
+# Declaration of functions
+print("blueberry")
+
+print(fruits[5:10])
+
+fruits[6] = "onion"
+print(fruits)
+
+# Main
+
+
+# End
